b7852e71a32431dd7e89ef68f8c57ca0389d8ad3
maniacboy777 <rkm0722@gmail.com>
2024-02-22 13:56:01 -0700

    complete centos.sh

---
+++ releases/centos.sh
@@ -0,0 +1,7 @@
+DISTRO_NAME="CentOS"
+DISTRO_COMMENT="Source: EXALAB's Anlinux"
+
+TARBALL_URL['aarch64']="https://github.com/EXALAB/Anlinux-Resources/raw/master/Rootfs/CentOS_Stream/arm64/centos_stream-rootfs-arm64.tar.xz"
+TARBALL_SHA256['aarch64']="a87396d023db2b3dd0f58808464e0adf76fc9b857cb44f3666242c9641e5e5aa"
+TARBALL_URL['x86_64']="https://github.com/EXALAB/Anlinux-Resources/raw/master/Rootfs/CentOS_Stream/amd64/centos_stream-rootfs-amd64.tar.xz"
+TARBALL_SHA256['x86_64']="09e6a56a594fb64dd8bcd829dd3941644ad14cf5599f4c431b1c3381f1e6072f"
