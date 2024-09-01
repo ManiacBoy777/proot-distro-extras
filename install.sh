a6a175c59c01f772f25a57fcbc9a1bd7027cc4bd
maniacboy777 <rkm0722@gmail.com>
2024-08-31 21:12:23 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ echo -e "${BLUE}nethunter"
 echo -e "${BLUE}parrot"
 echo -e "${BLUE}centos"
 echo ''
-echo -e "Type ${GREEN}proot-distro${RESET} list to show a list of all available distributions"
+echo -e "${RESET}Type ${GREEN}proot-distro${RESET} list to show a list of all available distributions"
 echo ''
 echo -e "Then type ${GREEN}proot-distro install <alias>${RESET} to install it"
 echo ''
