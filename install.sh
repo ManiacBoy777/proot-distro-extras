42325f0a95df3f8fc65e9d715eece9d4d24efb92
maniacboy777 <rkm0722@gmail.com>
2024-08-31 20:12:33 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+curl -fsSL "https://raw.githubusercontent.com/ManiacBoy777/proot-distro-extras/master/releases/kali.sh" -o $PREFIX/etc/proot-distro/kali.sh
+
+echo proot-distro-extras is now installed!
+echo ''
+echo ''
+echo ''
+echo Type 'proot-distro list' to show a list of all available distributions
+echo then type 'proot-distro install <alias>' to install it
+echo ''
+echo Example: proot-distro install kali
+echo ''
+echo enjoy!
